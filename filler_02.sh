18187a204ca18eac2626ebea07f9575efdc32d43
C. Herrera <pro.cah@gmail.com>
2016-07-31 13:43:00 -0500

    Small code op for the main routine

--- filler_02.sh
+++ filler_02.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# 	File          : filler_01.sh
+# 	File          : filler_02.sh
 #	Date          : 05-31-2016
 #	Program name  : space filler
-#	Version       : 0.0.1
+#	Version       : 0.0.2
 #	Author        : ----
 #	Enviroment    : CLI
 #	Description   : This little script will try to create several
@@ -11,12 +11,36 @@
 #	Notes         : Not optimized at all, just to test the speed of my system
 #
 #
-# 
+#
+# Some vars to start
+today=$(date)
+host=$(hostname)
+linuxver=$(uname -r)
+filler_version=0.0.2
+PCT=0
+# the data to copy
+datatofile="1010101010101010101010101010101"
+limit=0
+# Case vars to catch from dialog
+DIALOG_CANCEL=1
+DIALOG_ESC=255
+HEIGHT=0
+WIDTH=0
+# Width and height of the gauge window
+RHEIGHT=22
+RWIDTH=77
+# The ouput temp file to use
+OUTPUT=$HOME/output.temp$$   #/home/versus/bin/output.tmp$$
+TMPFILE=$HOME/testfile.tmp$$ #/home/versus/bin/testfile.tmp$$
+
+# trap and delete temp files
+trap "rm $OUTPUT; rm $TMPFILE; exit" SIGHUP SIGINT SIGTERM
 
 function copy_test_1(){
 	echo "---------------------------------------------------------"
 	echo " This test started at : $today"
 	echo " First test , write multiple files using a fixed 32 character line"
+	echo " The test will run for $limit times"
 	for (( i=0; i <limit; i++))
 	do
 		echo " Creating folder 'folder_$i'"
@@ -25,13 +49,10 @@ function copy_test_1(){
 		for (( b= 0; b < limit; b++))
 		do
 			touch "file_"$b
-			#echo " File 'file_$b' was created"
 			for (( f=0; f < 128; f++))
 			do
 				echo $datatofile >> "file_"$b
 			done
-			#printf "File created at %s" $(date)
-			#echo "File 'file$b' was created inside $(pwd)"
 		done
 		echo " Done with 'folder_$i' at $(date +%I:%M:%S:%N)"
 		cd ..
@@ -44,7 +65,7 @@ function copy_test_1(){
 function copy_test_2(){
 	echo "---------------------------------------------------------"
 	echo " Second test, copying of a created file of 4.0 KiB lenght"
-	echo " This test will use the same options from last test"
+	echo " The test will run for $limit times"
 	for (( r = 0; r < limit; r++))
 	do
 		echo " Copying to folder_$r"
@@ -62,7 +83,7 @@ function copy_test_2(){
 	function copy_test_3(){
 	echo "---------------------------------------------------------"
 	echo " Third test, copying of a created file of 8.0 KiB lenght"
-	echo " This test will use the same options from last test"
+	echo " The test will run for $limit times"
 	for (( r = 0; r < limit; r++))
 	do
 		echo " Copying to folder_$r"
@@ -79,7 +100,7 @@ function copy_test_2(){
 function copy_test_4(){
 	echo "---------------------------------------------------------"
 	echo " Fourth test, copying of a created file of 16.0 KiB lenght"
-	echo " This test will use the same options from last test"
+	echo " The test will run for $limit times"
 	for (( r = 0; r < limit; r++))
 	do
 		echo " Copying to folder_$r"
@@ -96,7 +117,7 @@ function copy_test_4(){
 function copy_test_5(){
 	echo "---------------------------------------------------------"
 	echo " Fifth test, copying of a created file of 32.0 KiB lenght"
-	echo " This test will use the same options from last test"
+	echo " The test will run for $limit times"
 	for (( r = 0; r < limit; r++))
 	do
 		echo " Copying to folder_$r"
@@ -113,7 +134,7 @@ function copy_test_5(){
 function copy_test_6(){
 	echo "---------------------------------------------------------"
 	echo " Sixth test, copying of a created file of 256.0 KiB lenght"
-	echo " This test will use the same options from last test"
+	echo " The test will run for $limit times"
 	for (( r = 0; r < limit; r++))
 	do
 		echo " Copying to folder_$r"
@@ -127,36 +148,12 @@ function copy_test_6(){
 	echo " Test ended at '$today'"
 }
 clear
-# Some vars to start
-today=$(date)
-host=$(hostname)
-linuxver=$(uname -r)
-PCT=0
-# the data to copy
-datatofile="1010101010101010101010101010101"
-limit=8
-# Case vars to catch from dialog
-DIALOG_CANCEL=1
-DIALOG_ESC=255
-HEIGHT=0
-WIDTH=0
-# Width and height of the gauge window
-RHEIGHT=22
-RWIDTH=77
-# The ouput temp file to use
-OUTPUT=$HOME/output.temp$$   #/home/versus/bin/output.tmp$$
-TMPFILE=$HOME/testfile.tmp$$ #/home/versus/bin/testfile.tmp$$
-
-# trap and delete temp files
-trap "rm $OUTPUT; rm $TMPFILE; exit" SIGHUP SIGINT SIGTERM
-
-
 
 while true; do
 	PCT=17
 	exec 3>&1
 	selection=$(dialog \
-		--backtitle "Simple test for the filesystem" \
+		--backtitle "Copy test for Storage Units ver. $filler_version" \
 		--title "Main Menu" \
 		--clear \
 		--cancel-label "Exit" \
@@ -187,102 +184,42 @@ while true; do
 	case $selection in
 		0 )
 			clear
-			echo "Program terminated."
+			echo "Program terminated 1."
 		;;
 		1 )
 			limit=32
-			(
-			copy_test_1 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			testprogress=" Test 1 ended "
-			copy_test_2 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			copy_test_3 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			copy_test_4 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			copy_test_5 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			copy_test_6 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			) | dialog --title "Test progress" --gauge "Please wait while test ends..." 20 60 0
-			cat $OUTPUT | expand >> $TMPFILE
-			dialog --backtitle "Sequential Byte Copy test" --title "Results" --textbox "$TMPFILE" $RHEIGHT $RWIDTH
 		;;
 		2 )
 			limit=64
-			(
-			copy_test_1 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			copy_test_2 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			copy_test_3 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			copy_test_4 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			copy_test_5 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			copy_test_6 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			) | dialog --title "Test progress" --gauge "Please wait while test ends..." 20 70 0
-			cat $OUTPUT | expand >> $TMPFILE
-			dialog --backtitle "Sequential Byte Copy test" --title "Results" --textbox "$TMPFILE" $RHEIGHT $RWIDTH
 		;;
 		3 )
 			limit=128
-			(
-			copy_test_1 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			copy_test_2 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			copy_test_3 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			copy_test_4 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			copy_test_5 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			copy_test_6 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			) | dialog --title "Test progress" --gauge "Please wait while test ends..." 20 70 0
-			cat $OUTPUT | expand >> $TMPFILE
-			dialog --backtitle "Sequential Byte Copy test" --title "Results" --textbox "$TMPFILE" $RHEIGHT $RWIDTH
 		;;
 		4 )
 			limit=256
-			(
-			copy_test_1 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			copy_test_2 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			copy_test_3 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			copy_test_4 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			copy_test_5 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			copy_test_6 >> $OUTPUT
-			PCT=`expr $PCT + 10`
-			echo $PCT
-			) | dialog --title "Test progress" --gauge "Please wait while test ends..." 20 70 0
-			cat $OUTPUT | expand >> $TMPFILE
-			dialog --backtitle "Sequential Byte Copy test" --title "Results" --textbox "$TMPFILE" $RHEIGHT $RWIDTH
 		;;
 	esac
+		(
+		copy_test_1 >> $OUTPUT
+		PCT=`expr $PCT + 10`
+		echo $PCT
+		testprogress=" Test 1 ended "
+		copy_test_2 >> $OUTPUT
+		PCT=`expr $PCT + 10`
+		copy_test_3 >> $OUTPUT
+		PCT=`expr $PCT + 10`
+		echo $PCT
+		copy_test_4 >> $OUTPUT
+		PCT=`expr $PCT + 10`
+		copy_test_5 >> $OUTPUT
+		PCT=`expr $PCT + 10`
+		echo $PCT
+		copy_test_6 >> $OUTPUT
+		PCT=`expr $PCT + 10`
+		echo $PCT
+		) | dialog --title "Test progress" --gauge "Please wait while test ends..." 20 60 0
+		cat $OUTPUT | expand >> $TMPFILE
+		dialog --backtitle "Sequential Byte Copy test" --title "Results" --textbox "$TMPFILE" $RHEIGHT $RWIDTH
 done
 
 
@@ -292,4 +229,6 @@ done
 [ -f $TMPFILE ] && rm $TMPFILE
 #set +x
 exit
+
+
 
